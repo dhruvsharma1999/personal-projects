743c6adb30b0676600c927b3908b1955d54bed0d
dhruvsharma1999 <dhruvshrm34@gmail.com>
2020-12-20 19:08:27 +0530

    completed CRUD functionality

--- yelpcamp/app.js
+++ yelpcamp/app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
+const methodOverride = require('method-override');
 const Campground = require('./models/campground');
+
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', 
 {
@@ -22,6 +24,9 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+app.use(express.urlencoded({extended: true}));
+app.use(methodOverride('_method'));
+
 
 app.get('/', (req, res) =>{
     res.render('home')
@@ -32,13 +37,40 @@ app.get('/campgrounds', async (req, res) => {
     res.render('campgrounds/index', {campgrounds})
 })
 
+app.get('/campgrounds/new', (req, res) => {
+    res.render('campgrounds/new');
+})
+
+app.post('/campgrounds', async (req, res) => {
+    
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`)
+})
+
+
 app.get('/campgrounds/:id', async (req, res) => {
     const campground = await Campground.findById(req.params.id)
-    res.render('campgrounds/show', {campground})
+    res.render('campgrounds/show', {campground});
 })
-  
 
+app.get('/campgrounds/:id/edit', async (req, res) => {
+    const campground = await Campground.findById(req.params.id)
+    res.render('campgrounds/edit', {campground});
+})
 
-app.listen(5000, () => {
-    console.log('Serving on port 5000')
+app.put('/campgrounds/:id', async (req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
+    res.redirect(`/campgrounds/${campground._id}`)
+});
+
+app.delete('/campgrounds/:id', async (req, res) => {
+    const {id} = req.params;
+    await Campground.findByIdAndDelete(id);
+    res.redirect('/campgrounds');
+})
+
+app.listen(5050, () => {
+    console.log('Serving on port 5050')
 })
