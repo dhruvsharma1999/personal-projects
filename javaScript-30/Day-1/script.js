167321932b67bd083889f49a6be5241d3c27142e
dhruvsharma1999 <dhruvshrm34@gmail.com>
2021-06-07 16:50:19 +0530

    added JS30 D1

---
+++ javaScript-30/Day-1/script.js
@@ -0,0 +1,28 @@
+"strict";
+
+/*
+    1. Listning to keyboard event.
+    2. if that key has a data-key attribute equal to key code, then we play the sound associated with it. 
+    2. adding playing class when the corresponsing key is pressed/hit.
+
+
+*/
+
+function removeTransition(e) {
+  if (e.propertyName !== "transform") return;
+  e.target.classList.remove("playing");
+}
+
+function playSound(e) {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+  if (!audio) return;
+
+  key.classList.add("playing");
+  audio.currentTime = 0;
+  audio.play();
+}
+
+const keys = Array.from(document.querySelectorAll(".key"));
+keys.forEach((key) => key.addEventListener("transitionend", removeTransition));
+window.addEventListener("keydown", playSound);
